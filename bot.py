7a691b06d03575a8765afeb9268c6eff16facfe4
Олег Ишин <oleg.ishin@gmail.com>
2025-08-09 07:44:24 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! Я FotiniaBot.")
+
+def main():
+    token = os.environ.get("TELEGRAM_TOKEN")
+    if not token:
+        print("Ошибка: нужно установить TELEGRAM_TOKEN")
+        return
+
+    app = ApplicationBuilder().token(token).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
